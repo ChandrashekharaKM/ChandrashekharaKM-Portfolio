afa99445d971f8dd61f10d9ca1f95fb996fec7d1
ChandrashekharaKM <chandrashekharakm07@gmail.com>
2026-05-17 13:06:53 +0530

    chore: remove profile overlay text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,6 @@ const About = () => {
               <div className="profile-ring animate-float-ring">
                 <div className="profile-frame">
                   <img src={ProfileImage} alt="Chandrashekhara KM" onContextMenu={(e) => e.preventDefault()} />
-                  <div className="image-overlay">Do not download image</div>
                 </div>
               </div>
               <div className="profile-details">
